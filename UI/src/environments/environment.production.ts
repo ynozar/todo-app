9edf9d17f8d838ed545d25bdcf9454b2ce6ef0b6
Yoel <22827013+ynozar@users.noreply.github.com>
2024-06-05 08:47:26 -0700

    add backend link

--- UI/src/environments/environment.production.ts
+++ UI/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://af0db5ec9c2224243814338b41327463-1843528983.us-east-1.elb.amazonaws.com",
+  apiUrl: "https://backend.blueforest-e780f834.eastus.azurecontainerapps.io",
 };
